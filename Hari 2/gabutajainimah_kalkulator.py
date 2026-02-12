09f23c8dcb8f5f48f670225068f8ad70b3934815
ikikiye <muhammadrisqi103@gmail.com>
2026-02-12 16:30:28 +0700

    gatau ini mlaha ngerjain yg gaperlu

--- Hari 2/gabutajainimah_kalkulator.py
+++ Hari 2/gabutajainimah_kalkulator.py
@@ -1,28 +1,45 @@
+import os
 import random as rd
 
-print("=============Kalkulator yang tidak berguna=================")
+def main():
+    os.system('clear')
+    print("=============Kalkulator yang tidak berguna=================")
 
-a=int(input("Tuliskan Angka Pertama = "))
-b=int(input("Tuliskan Angka Kedua = "))
-c=input("Tuliskan Operator (x, :, +, -) = ")
-randomNum = rd.randrange(1,a+b)
+    a=int(input("Tuliskan Angka Pertama = "))
+    b=int(input("Tuliskan Angka Kedua = "))
 
-if c == "x" and a*b != randomNum:
-    Hasil = a*b
-    print("Bukan Hasilnya =", randomNum)
-    #print(Hasil)
-elif c == ":" and a*b != randomNum:
-    Hasil = a/b
-    print("Bukan Hasilnya =", randomNum)
-    #print(Hasil)
-elif c == "+" and a*b != randomNum:
-    Hasil = a+b
-    print("Bukan Hasilnya =", randomNum)
-    #print(Hasil)
-elif c == "-" and a*b != randomNum:
-    Hasil = a-b
-    print("Bukan Hasilnya =", randomNum)
-    #print(Hasil)
-else:
-    print("        FLASHBANG        ")
-    print("===== ehdn of program =====")
+    c=input("Tuliskan Operator (x, :, +, -) = ")
+    randomNum = rd.randrange(1,1000)
+
+    if c == "x" and a*b != randomNum:
+        Hasil = a*b
+        print("Bukan Hasilnya =", randomNum)
+        #print(Hasil)
+    elif c == ":" and a*b != randomNum:
+        Hasil = a/b
+        print("Bukan Hasilnya =", randomNum)
+        #print(Hasil)
+    elif c == "+" and a*b != randomNum:
+        Hasil = a+b
+        print("Bukan Hasilnya =", randomNum)
+        #print(Hasil)
+    elif c == "-" and a*b != randomNum:
+        Hasil = a-b
+        print("Bukan Hasilnya =", randomNum)
+        #print(Hasil)
+    else:
+        print("        FLASHBANG        ")
+        print("===== ehdn of program =====")
+
+while True:
+    main()
+    konfirmasi = input("Apakah ingin lanjut? (y/n) :").strip().upper()
+
+    if konfirmasi == "Y":
+        alif = 4
+    elif konfirmasi == "N":
+        os.system('clear')
+        break
+    else:
+        print("woi yang bener ngetiknya")
+        input()
--- Hari 2/tugas-1.py
+++ Hari 2/tugas-1.py
@@ -1,7 +1,7 @@
-print("===== Kalkulator Perkalian =====")
+print("===== Kalkulator Pengurangan =====")
 
 a = int(input("Masukkan Nilai a = "))
 b = int(input("Masukkan Nilai b = "))
-print("Hasil dari perkalian", a, "dan", b, "adalah =",a*b)
+print("Hasil dari pengurangan", a, "dan", b, "adalah =",a-b)
 
 print("===== Akhir dari program =====")
